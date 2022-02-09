5a52b4d21f22e6101d1512def2ecdbc3f0b8102f
chateauxvt <tolgaoa@vt.edu>
2022-02-08 21:04:24 -0500

    transfer to new comp

--- src/xrfapp/xrf_main.cpp
+++ src/xrfapp/xrf_main.cpp
@@ -41,28 +41,26 @@ void xrf_main::access_token_request(
 		int& http_code, const uint8_t http_version, 
 		ProblemDetails& problem_details){
 
-	std::map<std::string, std::string> access_token_req;
-	std::vector<std::string> key_values;
-	boost::split(key_values, request_main, boost::is_any_of("&"), boost::token_compress_on);
+	std::map<std::string, std::string> request;
+	std::vector<std::string> kvpairs;
+	boost::split(kvpair, request_main, boost::is_any_of("&"), boost::token_compress_on);
 
-	for (auto i : key_values){
-		std::vector<std::string> key_value;
-		boost::split(key_value, i, boost::is_any_of(","), boost::token_compress_on);
-		if (key_value.size() != 2){
+	for (auto i : kvpairs){
+		std::vector<std::string> kvpair;
+		boost::split(kvpair, i, boost::is_any_of(","), boost::token_compress_on);
+		if (kvpair.size() != 2){
 			std::cout << "Invalid Request" << std::endl;
-			//Logger::xrf_main().debug("Invalid request");
-		}else access_token_req[key_value[0]] = key_value[1];	
+		}else access_token_req[kvpair[0]] = kvpair[1];	
 
-		//Logger::xrf_main().debug("(Key, value): %s, %s", value[0].c_str(), value[1].c_str());
 		printf("(Key, Value):  %s, %s \n", key_value[0].c_str(), key_value[1].c_str());
 	}
 
-	//Generate the JWT object
-	std::string signature = {};
+	//JWT Object created here
+	std::string sign = {};
 	bool outcome = false;
 
-	outcome = xrf_jwt_inst->generate_signature("00001", "1", "00002", "A1", signature);
-	std::cout << signature << std::endl;
+	outcome = xrf_jwt_inst->generate_signature("00001", "1", "00002", "A1", sign);
+	std::cout << sign << std::endl;
 
 	access_token_rsp.setAccessToken(signature);
 	access_token_rsp.setTokenType("Bearer");
