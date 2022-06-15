bae51fd56565eee2f32de41c2da8200d8f620068
chateauxvt <tolgaoa@vt.edu>
2022-06-15 18:10:01 -0400

    merge test

--- xrfclient/src/main.cpp
+++ xrfclient/src/main.cpp
@@ -19,7 +19,7 @@ int main(int argc, char** argv){
 	//Set log level debug
 	spdlog::set_level(spdlog::level::debug);
 
-	const std::string xrfaddress = "http://192.168.70.129:9090/init/auth";
+	const std::string xrfaddress = "http://192.168.3.129:9090/init/auth";
 	const std::string xrfchallenge = "Sudip's String A" ;
 	spdlog::info("Sending Initial Authentication Challenge to XRF");
 	xapp_main_inst->sendauth_to_xrf(xrfchallenge, xrfaddress);
